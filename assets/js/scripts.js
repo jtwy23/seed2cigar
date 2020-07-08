67e592038c327a44e417e98f78b9334242981d00
jtwy23 <jtwy_23@hotmail.com>
2020-07-08 17:15:12 +0000

    Alphabetical Nav List javascript code has been added to scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,4 @@
+// Start of Accordion 
 var acc = document.getElementsByClassName("accordion");
 var i;
 
@@ -16,3 +17,42 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
+// End of Accordion
+
+// Start of Alphabetical Nav List
+let createArrayAtoZ = _ => {
+            return Array 
+               .apply(null, {length: 26}) 
+               .map((x, i) => String.fromCharCode(65 + i));
+        }
+
+        let jumptoAnchor = anchor => {
+            window.location.href = "#" + anchor;
+        }
+
+        let createNavigationList = _ => {
+            const abcChars = createArrayAtoZ();
+            const navigationEntries = abcChars.reduce(createDivForCharElement, ''); 
+            $('#nav').append(navigationEntries);
+
+            const lettersActive = ['A', 'C', 'G', 'M', 'X'];
+            lettersActive.forEach(letter => { 
+               changeItemState(letter); 
+               addListEntries(letter); 
+            });
+        }
+
+        let changeItemState = character => {
+            const characterElement = $('#nav').find('.CharacterElement[data-filter="' + character + '"]');
+            $(characterElement).click(() => jumptoAnchor(character));
+            characterElement.removeClass('Inactive');
+        }
+
+        let createDivForCharElement = (block, charToAdd) => { 
+            return block + "<div id='CharacterElement' class='CharacterElement Inactive' data-filter='" + charToAdd + "'>" + charToAdd + "</div>"; 
+        }
+
+        let addListEntries = letter => {
+            $('#AppComponent').append("<div class='AppElement' id='" + letter + "'>" + letter + "</div>");
+        }
+// End of Alphabetical Nav List
