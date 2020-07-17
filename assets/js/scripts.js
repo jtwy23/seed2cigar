da7c149c8bfd951ad45806fcc5b471c09b902b22
jtwy23 <jtwy_23@hotmail.com>
2020-07-17 00:01:05 +0000

    Changed the Quiz js code to set the word position

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -9,7 +9,7 @@ function navCharacters(id) {
 
 // End of Cigar Talk
 
-// Cigar Quiz JS
+// Start of Cigar Quiz JS
 function Quiz(questions) {
     this.score = 0;
     this.questions = questions;
@@ -47,11 +47,11 @@ function populate() {
         showScores();
     }
     else {
-        // show question
+        // Show Question
         var element = document.getElementById("question");
         element.innerHTML = quiz.getQuestionIndex().text;
  
-        // show options
+        // Show Options
         var choices = quiz.getQuestionIndex().choices;
         for(var i = 0; i < choices.length; i++) {
             var element = document.getElementById("choice" + i);
@@ -84,20 +84,22 @@ function showScores() {
     element.innerHTML = gameOverHTML;
 };
  
-// create questions here
+// Create Questions Here
 var questions = [
     new Question("How does a cigar begin its journey?", ["When a cigar smoker lights a cigar up", "When the cigar is being rolled in a factory","As a cigar seed to be planted"], "As a cigar seed to be planted"),
     new Question("How many days does it take for the planted seed to become a seedling?", ["40 Days", "60 Days", "80 Days"], "60 Days"),
     new Question("How long does it take the seedling to mature to a full plant?", ["2 Months", "6 Months","12 Months"], "2 Months"),
-    new Question("In which direction is the tobacco plants matured?", ["From the bottom of the plant upwards", "From the top of the plant downwards", "From the centre of the plant outwards"], "From the bottom of the plant upwards"),
+    new Question("In which direction is the tobacco plants matured?", ["Bottom, upwards", "Top, downwards", "Center, outwards"], "Bottom, upwards"),
     new Question("How heavy are the cigar bales?", ["20lbs", "2000lbs", "200lbs"], "200lbs"),
-    new Question("What is a humidor?", ["A box (or even a room) used to keep cigars stored in the ideal conditions", "A cigar that is made only using tobacco from Cuba", "A type of filler tobacco with a bold flavour. This lead comes from the top of the plant"], "A box (or even a room) used to keep cigars stored in the ideal conditions"),
+    new Question("What is a humidor?", ["A box to keep cigars", "A Cuban box", "Type of filler tobacco"], "A box to keep cigars"),
     new Question("A torcedor can roll up to 500 cigars a day. But what is the normal amount they roll?", ["150 Cigars", "300 Cigars", "500 Cigars"], "150 Cigars"),
-    new Question("Why is freshly cured tobacco not ready to smoke?", ["The leaves are too wet still", "There are too many bugs on the leaves", "It has a lot of ammonia and other impurites still in the leaves"], "It has a lot of ammonia and other impurites still in the leaves")
+    new Question("Why is freshly cured tobacco not ready to smoke?", ["The leaves are too wet", "Too many bugs", "Too much ammonia"], "Too much ammonia")
 ];
  
-// create quiz
+// Create Quiz
 var quiz = new Quiz(questions);
  
-// display quiz
+// Display Quiz
 populate();
+
+// End of Cigar Quiz
