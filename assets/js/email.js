2bf970591455a219bdab3afe5aeeebf4183601e4
jtwy23 <jtwy_23@hotmail.com>
2020-07-24 21:30:00 +0000

    Code error correction. Email messages now send properly to email addres with message

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,13 +1,13 @@
 function sendMail(contact_form) {
     emailjs.send("gmail", "seed2cigar", {
-        "from_name": contact_form.from_name_value,
-        "from_email": contact_form.from_email_value,
-        "ask_question": contact_form.ask_question_value
+        "from_name": contact_form.from_name.value,
+        "from_email": contact_form.from_email.value,
+        "ask_question": contact_form.ask_question.value
     })
     .then(
         function(response) {
-            console.log("SUCCESS", response);
             document.getElementById("contact_form").reset();
+            console.log("SUCCESS", response);
         },
         function(error) {
             console.log("FAILED", error);
