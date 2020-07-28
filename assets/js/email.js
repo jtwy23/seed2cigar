22bb55478851bfaa663935fd8b6e2cad9734f252
jtwy23 <jtwy_23@hotmail.com>
2020-07-28 17:50:08 +0000

    Added a title comment

--- assets/js/email.js
+++ assets/js/email.js
@@ -29,17 +29,17 @@ var span = document.getElementsByClassName("close")[0];
 // When the user clicks on the button, open the modal box
 btn.onclick = function() {
   modal.style.display = "block";
-}
+};
 
 // When the user clicks on <span> (x), close the modal box
 span.onclick = function() {
   modal.style.display = "none";
-}
+};
 
 // When the user clicks anywhere outside of the modal box, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
-}
+};
 
