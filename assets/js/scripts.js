1e0bf14b8043eeb1316a16e78662269bd1b717dc
jtwy23 <jtwy_23@hotmail.com>
2020-07-28 17:51:22 +0000

    Added a title comment

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,12 +1,14 @@
+// Scripts
+
 // Navbar Fix now scrolls to each section
 $(".nav-link").on("click", function(event){
     event.preventDefault();
     $('html, body').animate({
          scrollTop: $($(this).attr("href")).offset().top - 100
      }, 400);
-     $(".nav-link").removeClass("active")
+     $(".nav-link").removeClass("active");
      $(this).addClass("active");
-} )
+} );
 
 // Cigar Talk will show individual letters of each definition
 function navCharacters(id) {
